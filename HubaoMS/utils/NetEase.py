656a8a436daba80a435907d5b7be7b01ad5bd543
Jayden Chen <chenzhenhaojayden@gmail.com>
2016-11-28 16:59:39 +0800

    refresh netease nickname

--- HubaoMS/utils/NetEase.py
+++ HubaoMS/utils/NetEase.py
@@ -61,8 +61,23 @@ class NetEase(object):
 
 
 if __name__ == '__main__':
+    import random
+    from config import HOST, PORT, USER, PASSWORD, DATABASE
+    import psycopg2
     net = NetEase()
-    print requests.post("https://api.netease.im/nimserver/user/updateUinfo.action", data=urlencode({"accid":"system", "name":u"系统广播".encode("utf-8")}), headers=net.generate_header()).content
-    print requests.post("https://api.netease.im/nimserver/user/getUinfos.action", data=urlencode({"accids":["system"]}), headers=net.generate_header()).content
+
+    # conn = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, database=DATABASE)
+    conn = psycopg2.connect(host="10.116.108.164", port=5432, user="postgres", password="L4K3J4Y72yLy", database="hubao_tv_db")
+    cur = conn.cursor()
+    cur.execute("select username from chat_account c left join users u on c.id=replace(u.uuid::\"varchar\", '-', '') where uuid is null")
+    # print len(cur.fetchall())
+
+    # for username in cur.fetchall():
+    #     r = requests.post("https://api.netease.im/nimserver/user/updateUinfo.action", data=urlencode({"accid":username[0], "name":u"游客{}".format(random.randint(100000, 999999)).encode("utf-8")}), headers=net.generate_header())
+    #     if r.status_code != 200:
+    #         print r.content
+    # net = NetEase()
+    # print requests.post("https://api.netease.im/nimserver/user/updateUinfo.action", data=urlencode({"accid":"system", "name":u"系统广播".encode("utf-8")}), headers=net.generate_header()).content
+    # print requests.post("https://api.netease.im/nimserver/user/getUinfos.action", data=urlencode({"accids":["system"]}), headers=net.generate_header()).content
     pass
 
